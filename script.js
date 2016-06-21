3a34118d6344190d8f66ec62624f797ccb330293
Frank Ali <fali@sapient.com>
2016-06-20 20:43:49 -0400

    Adding starter code.

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("form-sayThatNumber").addEventListener("submit", sayThatNumber);
+
+function sayThatNumber(event){
+    event.preventDefault();
+    //enter your code here
+}
